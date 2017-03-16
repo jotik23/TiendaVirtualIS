c99fa1e618b29fb8595dd1a6ae63e258a7c1e5b6
jotik23 <jhonjvergarab@gmail.com>
2017-03-16 17:52:33 -0500

    UI Backend and Frontend

--- TiendaVirtualIS/src/co/edu/unac/ing/store/controllers/IndexServlet.java
+++ TiendaVirtualIS/src/co/edu/unac/ing/store/controllers/IndexServlet.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 /**
  * Created by lds on 16/03/2017.
  */
-@javax.servlet.annotation.WebServlet(name = "IndexServlet")
+@javax.servlet.annotation.WebServlet(name = "IndexServlet", urlPatterns="/Home")
 public class IndexServlet extends javax.servlet.http.HttpServlet {
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
 
