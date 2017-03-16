51293627ee9beac76c3d00a393dd457b11bc0476
jotik23 <jhonjvergarab@gmail.com>
2017-03-16 17:27:52 -0500

    Initial Setup

---
+++ TiendaVirtualIS/src/co/edu/unac/ing/store/controllers/IndexServlet.java
@@ -0,0 +1,17 @@
+package co.edu.unac.ing.store.controllers;
+
+import java.io.IOException;
+
+/**
+ * Created by lds on 16/03/2017.
+ */
+@javax.servlet.annotation.WebServlet(name = "IndexServlet")
+public class IndexServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
---
+++ TiendaVirtualIS/src/co/edu/unac/ing/store/dto/User.java
@@ -0,0 +1,7 @@
+package co.edu.unac.ing.store.dto;
+
+/**
+ * Created by lds on 16/03/2017.
+ */
+public class User {
+}
