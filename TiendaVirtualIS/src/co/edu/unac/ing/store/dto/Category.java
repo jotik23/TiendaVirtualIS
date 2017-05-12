d60128a33c9867c508d6e6d44da84460c2ec941f
brayantorres <brayantorres9431@gmail.com>
2017-05-12 11:57:26 -0500

    Cambios clase

---
+++ TiendaVirtualIS/src/co/edu/unac/ing/store/dto/Category.java
@@ -0,0 +1,27 @@
+package co.edu.unac.ing.store.dto;
+
+/**
+ * Created by brayandavid on 12/05/2017.
+ */
+public class Category {
+
+    private int id;
+    private String name;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- TiendaVirtualIS/src/co/edu/unac/ing/store/models/PersistenceCategories.java
+++
@@ -1,7 +0,0 @@
-package co.edu.unac.ing.store.models;
-
-/**
- * Created by brayandavid on 12/05/2017.
- */
-public class PersistenceCategories {
-}
