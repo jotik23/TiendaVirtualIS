4263a48c265d543dec761fdcfc4af19d596e8db9
brayantorres <brayantorres9431@gmail.com>
2017-05-12 10:38:18 -0500

    Consultar categorias

--- TiendaVirtualIS/src/co/edu/unac/ing/store/controllers/IndexController.java
+++ TiendaVirtualIS/src/co/edu/unac/ing/store/controllers/IndexController.java
@@ -6,23 +6,16 @@ import javax.servlet.RequestDispatcher;
 import java.io.IOException;
 
 /**
- * Created by lds on 16/03/2017.
+ * Created by brayandavid on 12/05/2017.
  */
-@javax.servlet.annotation.WebServlet(name = "IndexServlet", urlPatterns="/Home")
-public class IndexServlet extends javax.servlet.http.HttpServlet {
+@javax.servlet.annotation.WebServlet(name = "IndexController", urlPatterns="/ConsultaBD")
+public class IndexController extends javax.servlet.http.HttpServlet {
+
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
 
     }
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-
-        //Recuperar valores
-        //Hacer la lógica
-        //Conectarnos a la BD
-        Connection connection = new Connection();
-        //connection.connect("root", "", "store");
-        //Preparar datos
-        //Enviarlos al JSP
 
         RequestDispatcher RequetsDispatcherObj =request.getRequestDispatcher("/admin.jsp");
         RequetsDispatcherObj.forward(request, response);
